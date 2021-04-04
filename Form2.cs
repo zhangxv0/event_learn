7e3ac754d34dcf4c114f67c30c872431786d2fa9
zhangxv_0 <8153942+zhangxv0@user.noreply.gitee.com>
2021-04-04 21:08:32 +0800

    添加项目文件。

---
+++ Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public delegate void a1(string a);
+        public event a1 shijian;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = "这是第二个窗口";
+            if (this.shijian != null)
+            {
+                this.shijian.Invoke(a);
+            }
+        }
+    }
+}
